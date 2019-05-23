1864e36c754a0ec722870b005d5f77a324daada8
Jake Spector <spector.jake@gmail.com>
2019-05-23 13:09:56 +0800

    seeding db with ingredients

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'open-uri'
+require 'json'
+
+url = 'https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list'
+ingredients_serialized = open(url).read
+ingredient_list = JSON.parse(ingredients_serialized)
+
+ingredients = ingredient_list['drinks']
+
+ingredients.each do |ingredient|
+  Ingredient.create(name: ingredient['strIngredient1'])
+end
